6304e72f912418847ef643ad961400781e0d7109
Serhii Lyzun <1987boxior@gmail.com>
2025-03-14 23:15:55 +0200

    Remove obsolete comment in middleware function
    The removed comment was unnecessary and misleading, as the code already indicates its async behavior. This change improves code readability and maintains cleaner documentation.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import type {NextRequest} from "next/server";
 import {devAuthCheck} from "@/app/actions/devAuth";
 import {DEV_AUTH_NAME, DEV_AUTH_PATH} from "@/utils/constants/general.constants";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     const isDevAuth = await devAuthCheck();
     const isDevAuthPath = request.nextUrl.pathname.includes(DEV_AUTH_NAME);
