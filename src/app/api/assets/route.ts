c82cf9d0ae6ea520707d06adc6385b14b2536eef
Serhii Lyzun <1987boxior@gmail.com>
2025-04-03 11:18:43 +0300

    - test deploy

--- src/app/api/assets/route.ts
+++ src/app/api/assets/route.ts
@@ -4,7 +4,6 @@
 // To handle a GET request to /api/posts
 import {NextResponse} from "next/server";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(_req: Request) {
     return NextResponse.json(
         {a: "b"},
