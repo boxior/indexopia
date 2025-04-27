b855c12594ebc6832c3ae5f23f1ebbccb699eae2
Serhii Lyzun <1987boxior@gmail.com>
2025-04-27 12:17:02 +0300

    Add API endpoint for populating assets and their history
    Introduced a POST endpoint in `src/app/api/populate/route.ts`. It validates an API key and utilizes helper functions to manage assets and their history. Returns appropriate responses for success or failure cases.

---
+++ src/app/api/populate/route.ts
@@ -0,0 +1,49 @@
+import {NextResponse, NextRequest} from "next/server";
+import {ENV_VARIABLES} from "@/env";
+import {MAX_ASSET_COUNT, OMIT_ASSETS_IDS} from "@/utils/constants/general.constants";
+import {manageAssets, manageAssetsHistory} from "@/lib/db/helpers/db.helpers";
+
+/**
+ * Generate Default Custom Indexes
+ */
+export async function POST(req: NextRequest) {
+    try {
+        // Get the URL and search parameters
+        const {searchParams} = new URL(req.url);
+
+        // Retrieve the apiKey from the query string
+        const apiKey = searchParams.get("apiKey");
+
+        if (!apiKey) {
+            return NextResponse.json({error: "API key is missing"}, {status: 401});
+        }
+
+        // Validate the API key
+        if (apiKey !== ENV_VARIABLES.API_KEY) {
+            return NextResponse.json({error: "Invalid API key"}, {status: 403});
+        }
+
+        // Assets
+        const limit = MAX_ASSET_COUNT + OMIT_ASSETS_IDS.length;
+
+        await manageAssets({limit});
+
+        // Assets history
+        await manageAssetsHistory();
+
+        return NextResponse.json(
+            {success: true},
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            {data: JSON.parse(JSON.stringify(error))},
+            {
+                status: 400,
+            }
+        );
+    }
+}
