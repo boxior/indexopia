f16b70c960d4f237e354eb1011741cebe9d3e0cc
Serhii Lyzun <1987boxior@gmail.com>
2025-05-10 11:34:31 +0300

    Refactor IndexesPage to disable data fetching logic.
    Commented out the data fetching functions and replaced the returned JSX with a placeholder. This change simplifies the component temporarily, likely for debugging or development purposes.

--- src/app/indexes/page.tsx
+++ src/app/indexes/page.tsx
@@ -7,16 +7,18 @@ import * as React from "react";
 
 export default async function IndexesPage() {
     console.time("topIndexes");
-    const topIndexes = await Promise.all(Object.values(IndexId).map(id => getIndex({id})));
+    // const topIndexes = await Promise.all(Object.values(IndexId).map(id => getIndex({id})));
     console.timeEnd("topIndexes");
     console.time("customIndexes");
 
-    const customIndexes = await getCustomIndexes();
+    // const customIndexes = await getCustomIndexes();
     console.timeEnd("customIndexes");
     console.time("assets");
 
-    const assets = await getCachedTopAssets();
+    // const assets = await getCachedTopAssets();
     console.timeEnd("assets");
 
-    return <IndexesTable data={[...topIndexes, ...customIndexes]} assets={assets} />;
+    return <>Indexed Page</>;
+
+    // return <IndexesTable data={[...topIndexes, ...customIndexes]} assets={assets} />;
 }
