7b29d9beef4a3dfdec046925932ef83add958542
Serhii Lyzun <1987boxior@gmail.com>
2025-05-31 18:25:25 +0300

    Add MAX_DB_CONNECTIONS constant to manage database queries
    Introduce a MAX_DB_CONNECTIONS constant in general.constants.ts to limit the maximum number of database connections. Update the IndexesPage logic to slice the assets array based on this limit, ensuring optimized query execution and resource usage.

--- src/app/indexes/page.tsx
+++ src/app/indexes/page.tsx
@@ -5,6 +5,7 @@ import {getCachedTopAssets, getCustomIndexes} from "@/lib/db/helpers/db.helpers"
 import * as React from "react";
 import {SuspenseContainer} from "@/components/SuspenseContainer";
 import {dbQueryAssetHistoryById} from "@/lib/db/helpers/db.assetsHistory.helpers";
+import {MAX_DB_CONNECTIONS} from "@/utils/constants/general.constants";
 
 export default async function IndexesPage() {
     return (
@@ -20,7 +21,7 @@ const SuspendedComponent = async () => {
     // precache all histories, so that in the nested helpers it will be taken from cache as we use `use cache` directive.
     // Later, in any queries it will be taken from cache.
     await Promise.all(
-        assets.map(({id: assetId}) => {
+        assets.slice(0, MAX_DB_CONNECTIONS - 10).map(({id: assetId}) => {
             return (async () => {
                 try {
                     return dbQueryAssetHistoryById(assetId);
