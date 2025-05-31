bb3c9f93f789abcc459f4dbf4495d009668ed7ec
Serhii Lyzun <1987boxior@gmail.com>
2025-05-31 17:39:44 +0300

    Add custom headers to disable caching for all routes
    This update introduces a `headers` configuration to apply cache-control headers globally. It ensures no caching by setting `Cache-Control`, `Pragma`, and `Expires` headers. This change supports applications requiring fresh data on every request.

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,27 @@ const nextConfig: NextConfig = {
         dynamicIO: true,
     },
     compress: true,
+    async headers() {
+        return [
+            {
+                source: "/:path*", // Match all API routes for your app
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+                    },
+                    {
+                        key: "Pragma",
+                        value: "no-cache",
+                    },
+                    {
+                        key: "Expires",
+                        value: "0",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
