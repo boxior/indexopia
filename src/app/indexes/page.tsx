3bd7f6d55b85a709b1e8c08daa17778614d1c39f
Serhii <1987boxior@gmail.com>
2025-06-11 10:43:20 +0300

    Feature/scrum 23 optimize queries (#12)
    * Refactor DB connection, helpers, and environment variables
    
    Optimized MySQL pool settings and improved asset-fetching logic by filtering omitted IDs. Changed environment variable prefixes for consistency and adjusted error handling in custom index helpers to return defaults instead of throwing errors. Removed unused functions and updated cache-related logic for cleaner asset management.
    
    * Optimize asset history fetching with caching and refactoring
    
    Refactor asset history queries to use batch fetching and caching, improving performance and reducing repetitive database calls. Introduce `fetchAllAssetsAndHistory` for precaching assets and histories, and replace per-asset queries with cached data. Update related API routes and helpers to align with the new implementation.
    
    * Add database connection initialization in IndexesPage
    
    This ensures a connection is established before fetching data in SuspendedComponent. Improves reliability of data retrieval and prevents potential runtime issues.

--- src/app/indexes/page.tsx
+++ src/app/indexes/page.tsx
@@ -1,11 +1,10 @@
 "use server";
 
 import IndexesTable from "@/app/indexes/components/IndexesTable";
-import {getCachedTopAssets, getCustomIndexes} from "@/lib/db/helpers/db.helpers";
+import {fetchAllAssetsAndHistory, getCustomIndexes} from "@/lib/db/helpers/db.helpers";
 import * as React from "react";
 import {SuspenseContainer} from "@/components/SuspenseContainer";
-import {dbQueryAssetHistoryById} from "@/lib/db/helpers/db.assetsHistory.helpers";
-import {MAX_DB_CONNECTIONS} from "@/utils/constants/general.constants";
+import {connection} from "next/server";
 
 export default async function IndexesPage() {
     return (
@@ -16,23 +15,11 @@ export default async function IndexesPage() {
 }
 
 const SuspendedComponent = async () => {
-    const assets = await getCachedTopAssets();
+    await connection();
 
-    // precache all histories, so that in the nested helpers it will be taken from cache as we use `use cache` directive.
-    // Later, in any queries it will be taken from cache.
-    await Promise.all(
-        assets.slice(0, 5).map(({id: assetId}) => {
-            return (async () => {
-                try {
-                    return dbQueryAssetHistoryById(assetId);
-                } catch {
-                    return [];
-                }
-            })();
-        })
-    );
+    const {allAssets} = await fetchAllAssetsAndHistory();
 
     const customIndexes = await getCustomIndexes();
 
-    return <IndexesTable data={[...[], ...customIndexes]} assets={assets} />;
+    return <IndexesTable data={customIndexes} assets={allAssets} />;
 };
