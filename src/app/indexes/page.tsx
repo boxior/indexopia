1ffb1f4c6fb82df1d64a2daec7e9fc0d19cc60f6
Serhii Lyzun <1987boxior@gmail.com>
2025-06-11 12:26:22 +0300

    Remove unused prefetch logic for asset histories
    The prefetch logic for asset histories was commented out, as it is no longer used. This reduces redundant operations and improves code clarity. Future queries will still rely on caching where applicable.

--- src/app/indexes/page.tsx
+++ src/app/indexes/page.tsx
@@ -22,17 +22,17 @@ const SuspendedComponent = async () => {
 
     // // precache all histories, so that in the nested helpers it will be taken from cache as we use `use cache` directive.
     // // Later, in any queries it will be taken from cache.
-    await Promise.all(
-        assets.slice(0, 30).map(({id: assetId}) => {
-            return (async () => {
-                try {
-                    return dbQueryAssetHistoryById(assetId);
-                } catch {
-                    return [];
-                }
-            })();
-        })
-    );
+    // await Promise.all(
+    //     assets.slice(0, 5).map(({id: assetId}) => {
+    //         return (async () => {
+    //             try {
+    //                 return dbQueryAssetHistoryById(assetId);
+    //             } catch {
+    //                 return [];
+    //             }
+    //         })();
+    //     })
+    // );
 
     const customIndexes = await getCustomIndexes();
 
