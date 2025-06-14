7a1c72c43288e1b521e3e7d99476e0fb8f75cb85
Serhii Lyzun <1987boxior@gmail.com>
2025-06-14 16:43:38 +0300

    Remove API key validation from assets route
    The API key validation logic was removed from the `GET` handler in the assets route. This simplifies the endpoint by no longer requiring or verifying an API key in incoming requests. Ensure any external dependencies on this validation are addressed.

--- src/app/api/assets/route.ts
+++ src/app/api/assets/route.ts
@@ -46,21 +46,6 @@ export async function POST(_req: NextRequest) {
 
 export async function GET(_req: NextRequest) {
     try {
-        // Get the URL and search parameters
-        const {searchParams} = new URL(_req.url);
-
-        // Retrieve the apiKey from the query string
-        const apiKey = searchParams.get("apiKey");
-
-        if (!apiKey) {
-            return NextResponse.json({error: "API key is missing"}, {status: 401});
-        }
-
-        // Validate the API key
-        if (apiKey !== ENV_VARIABLES.API_KEY) {
-            return NextResponse.json({error: "Invalid API key"}, {status: 403});
-        }
-
         const assets = await dbGetAssets();
 
         return NextResponse.json(
