361bc82c65f9e2155c60b32c5221eda792d8b396
Serhii Lyzun <1987boxior@gmail.com>
2025-09-11 09:38:35 +0300

    Rename API route files to use kebab-case and add `warm-up` route for Vercel server initialization.

---
+++ src/app/api/warm-up/route.ts
@@ -0,0 +1,39 @@
+import {NextResponse, NextRequest} from "next/server";
+import {ENV_VARIABLES} from "@/env";
+
+/**
+ * It's necessary to warm up the Vercel server;
+ */
+export async function POST(req: NextRequest) {
+    try {
+        // Get the URL and search parameters
+        const {searchParams} = new URL(req.url);
+
+        // Retrieve the apiKey from the query string
+        const apiKey = searchParams.get("apiKey");
+
+        if (!apiKey) {
+            return NextResponse.json({error: "API key is missing"}, {status: 401});
+        }
+
+        // Validate the API key
+        if (apiKey !== ENV_VARIABLES.API_KEY) {
+            return NextResponse.json({error: "Invalid API key"}, {status: 403});
+        }
+
+        return NextResponse.json(
+            {success: true, date: new Date().toISOString()},
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            {data: JSON.parse(JSON.stringify(error))},
+            {
+                status: 400,
+            }
+        );
+    }
+}
