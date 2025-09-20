69dfb71fafd98879a35c45ddbc479a6a26a11f24
Serhii Lyzun <1987boxior@gmail.com>
2025-09-20 15:50:22 +0300

    Refactor API routes to reuse request handling logic and avoid redundant code in `populate` and `warm-up`.

--- src/app/api/warm-up/route.ts
+++ src/app/api/warm-up/route.ts
@@ -4,7 +4,8 @@ import {ENV_VARIABLES} from "@/env";
 /**
  * It's necessary to warm up the Vercel server;
  */
-export async function POST(req: NextRequest) {
+
+const handleRequest = async (req: NextRequest) => {
     try {
         // Get the URL and search parameters
         const {searchParams} = new URL(req.url);
@@ -36,38 +37,15 @@ export async function POST(req: NextRequest) {
             }
         );
     }
+};
+
+/**
+ * It's necessary to have POST request if here is the GET request to avoid pre-render issue during build project.
+ */
+export async function POST(req: NextRequest) {
+    await handleRequest(req);
 }
 
 export async function GET(req: NextRequest) {
-    try {
-        // Get the URL and search parameters
-        const {searchParams} = new URL(req.url);
-
-        // Retrieve the apiKey from the query string
-        const apiKey = searchParams.get("apiKey");
-
-        if (!apiKey) {
-            return NextResponse.json({error: "API key is missing"}, {status: 401});
-        }
-
-        // Validate the API key
-        if (apiKey !== ENV_VARIABLES.API_KEY) {
-            return NextResponse.json({error: "Invalid API key"}, {status: 403});
-        }
-
-        return NextResponse.json(
-            {success: true, date: new Date().toISOString()},
-            {
-                status: 200,
-            }
-        );
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json(
-            {data: JSON.parse(JSON.stringify(error))},
-            {
-                status: 400,
-            }
-        );
-    }
+    await handleRequest(req);
 }
