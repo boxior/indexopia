f0b72968bb8189637d933edde22494b1caa75fb9
Serhii Lyzun <1987boxior@gmail.com>
2025-10-10 01:08:23 +0300

    - deal with hook

--- src/app/[locale]/auth/signin/useReCaptcha.tsx
+++ src/app/[locale]/auth/signin/useReCaptcha.tsx
@@ -7,7 +7,7 @@ import {useLocale} from "next-intl";
 export const useReCaptcha = () => {
     const locale = useLocale();
 
-    useEffect(() => {
+    const attachReCaptcha = () => {
         // Create and append the script manually
         const script = document.createElement("script");
         script.src = `https://www.google.com/recaptcha/api.js?render=${ENV_VARIABLES.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}&hl=${locale}`;
@@ -15,15 +15,23 @@ export const useReCaptcha = () => {
         script.defer = true;
         script.id = "recaptcha-script";
         document.body.appendChild(script);
+    };
 
-        return () => {
-            // ✅ Remove script on unmount
-            const existingScript = document.getElementById("recaptcha-script");
-            if (existingScript) existingScript.remove();
+    const detachReCaptcha = () => {
+        // ✅ Remove script on unmount
+        const existingScript = document.getElementById("recaptcha-script");
+        if (existingScript) existingScript.remove();
 
-            // ✅ Clean up the injected reCAPTCHA elements
-            const recaptchaBadges = document.querySelectorAll(".grecaptcha-badge");
-            recaptchaBadges.forEach(badge => badge.remove());
-        };
+        // ✅ Clean up the injected reCAPTCHA elements
+        const recaptchaBadges = document.querySelectorAll(".grecaptcha-badge");
+        recaptchaBadges.forEach(badge => badge.remove());
+    };
+
+    useEffect(() => {
+        detachReCaptcha();
+
+        attachReCaptcha();
+
+        return detachReCaptcha;
     }, [locale]);
 };
