3cf784683bba8cbdef8e7ff4fb45c7da336baa1e
Serhii Lyzun <1987boxior@gmail.com>
2025-12-16 13:12:50 +0200

    - test release deployment

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -3,5 +3,6 @@
  * @source https://swr.vercel.app/docs/getting-started#quick-start
  * @param args
  */
+
 // @ts-ignore
 export const fetcher = (...args: any[]) => fetch(...args).then(res => res.json());
